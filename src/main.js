788ad0a260ac471f34df8a2e6be59cbf7ed1b850
htmelvis <ed@htmelvis.com>
2016-08-26 14:28:08 -0400

    clean and clear

--- src/main.js
+++ src/main.js
@@ -1,18 +1,2 @@
-import {inc, dec} from './actions';
 import $ from 'jquery';
 import './style/main.styl';
-
-
-window.onload = function() {
-  let number = document.getElementById('number');
-  let incBtn = document.getElementById('inc');
-  let decBtn = document.getElementById('dec');
-
-  incBtn.addEventListener('click', function() {
-    number.innerHTML = inc(+number.innerHTML);
-  }, false);
-
-  decBtn.addEventListener('click', function() {
-    number.innerHTML = dec(+number.innerHTML);
-  }, false);
-};
